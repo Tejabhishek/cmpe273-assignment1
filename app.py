6f567b2ef505a763995bf44fcc888920fd77f907
Tejabhishek <tejabhishek@gmail.com>
2017-03-18 00:45:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import sys
+from flask import Flask
+from github import Github
+from flask import json
+import yaml
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Dockerized Flask App!!"
+@app.route('/v1/<filename>')
+def config(filename):
+    path = sys.argv[1].split("/")
+    g = Github()
+    repo = g.get_user(path[3]).get_repo(path[4])
+    fileObj = repo.get_contents(filename)
+    ext = filename.split(".")
+    if (ext[1] == "yml" ):
+        cnt = (fileObj.content.decode('base64'))
+        return yaml.dump(cnt)
+    if (ext[1] == "json"):
+        cnt = (fileObj.content.decode('base64'))
+        return json.dumps(cnt)
 
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+if __name__=="__main__":
+    app.run(debug= True,host='0.0.0.0')
